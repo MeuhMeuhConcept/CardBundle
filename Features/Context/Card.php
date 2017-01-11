4b8a81d7a228d7c477dc7311735bb75a8a396cb0
mdeblaise <m.deblaise@meuhmeuhconcept.com>
2017-01-11 14:41:10 +0100

    Rename files

--- Features/Context/Card.php
+++ Features/Context/Card.php
@@ -6,6 +6,11 @@ use MMC\CardBundle\Entity\AbstractCard;
 
 class Card extends AbstractCard
 {
+    public function getId()
+    {
+        return 1;
+    }
+
     public function getSupportedClass()
     {
         return Item::class;
